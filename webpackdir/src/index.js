540fd49a04b93aceb163aabe179e56c61eb75c55
Aleksey <alekseibelsky@mail.ru>
2021-07-27 12:07:09 +0800

    Add webpackdir

---
+++ webpackdir/src/index.js
@@ -0,0 +1,9 @@
+console.log("WoW!")
+// index.js
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading)
