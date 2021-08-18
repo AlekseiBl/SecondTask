2b04474d0e557dcd7ecdfd0e5d9d5a9fc8ff4195
Aleksey <alekseibelsky@mail.ru>
2021-08-18 15:49:12 +0800

    Add dropdown

--- webpackdir/src/block/dropdown/dropdown.js
+++ webpackdir/src/block/dropdown/dropdown.js
@@ -1,6 +1,6 @@
 window.$ = window.jQuery = require( "jquery" );
-var iqDropdown = require("./item-quantity-dropdown.min.js");
-
+//var iqDropdown = require("./item-quantity-dropdown.min.js");
+//var iqDropdown = require("./index.js");
 
 $(document).ready(() => {
   $('.iqdropdown').iqDropdown({});
