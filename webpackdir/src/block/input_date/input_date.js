b121ac963f3bfb89c38d05762f638e7c7c652d05
Aleksey <alekseibelsky@mail.ru>
2021-09-03 14:57:25 +0800

    Fix Input & datepicker

--- webpackdir/src/block/input_date/input_date.js
+++
@@ -1,7 +0,0 @@
-import Inputmask from 'inputmask';
-
-export default Inputmask;
-
-Inputmask({mask: '99.99.9999', placeholder: 'ДД.ММ.ГГГГ'}).mask(
-    '.masked-input'
-  );
--- webpackdir/src/pages/index/index.js
+++ webpackdir/src/pages/index/index.js
@@ -2,3 +2,11 @@ function importAll(resolve) {
     resolve.keys().forEach(resolve);
 }
 importAll(require.context('../../../src/', true,/\.js$|\.scss$/));
+
+import Inputmask from 'inputmask';
+
+export default Inputmask;
+
+Inputmask({mask: '99.99.9999', placeholder: 'ДД.ММ.ГГГГ'}).mask(
+    '.input--masked'
+  );
