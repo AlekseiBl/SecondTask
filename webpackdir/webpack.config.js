63663dac6e52275ab1f1df55671281ac6db69278
Aleksey <alekseibelsky@mail.ru>
2021-10-29 16:44:58 +0800

    Added favicon webpack plugin

--- webpackdir/webpack.config.js
+++ webpackdir/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 var HtmlWebpackPugPlugin = require('html-webpack-pug-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 //const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
@@ -30,51 +31,66 @@ module.exports = {
             template: './src/pages/index/index.pug',
             filename: 'index.html',
             minify: false
-          }),
+        }),
         new HtmlWebpackPlugin({
             title: 'webpack Boilerplate',
             template: './src/pages/color&type/color&type.pug',
             filename: 'color&type.html',
             minify: false
-          }),
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/form-elements/form-elements.pug',
-            filename: 'form-elements.html',
-            minify: false
-          }),
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/cards/cards.pug',
-            filename: 'cards.html',
-            minify: false
-          }),  
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/headers&footers/headers&footers.pug',
-            filename: 'headers&footers.html',
-            minify: false
-          }),
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/landing/landing.pug',
-            filename: 'landing.html',
-            minify: false
-          }),
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/search-room/search-room.pug',
-            filename: 'search-room.html',
-            minify: false
-          }),
-          new HtmlWebpackPlugin({
-            title: 'webpack Boilerplate',
-            template: './src/pages/room-details/room-details.pug',
-            filename: 'room-details.html',
-            minify: false
-          }),
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/form-elements/form-elements.pug',
+          filename: 'form-elements.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/cards/cards.pug',
+          filename: 'cards.html',
+          minify: false
+        }),  
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/headers&footers/headers&footers.pug',
+          filename: 'headers&footers.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/landing/landing.pug',
+          filename: 'landing.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/search-room/search-room.pug',
+          filename: 'search-room.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/room-details/room-details.pug',
+          filename: 'room-details.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/registration/registration.pug',
+          filename: 'registration.html',
+          minify: false
+        }),
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: './src/pages/sign-in/sign-in.pug',
+          filename: 'sign-in.html',
+          minify: false
+        }),          
+        new FaviconsWebpackPlugin({
+          logo: './src/favicon2/favicon.png',
+          //mode: 'webapp',          
+        }),  
         new CleanWebpackPlugin(),
-        // применять изменения только при горячей перезагрузке
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPugPlugin()
     ],
@@ -88,12 +104,10 @@ module.exports = {
                     pretty:true,
                 }
               },
-            // изображения
             {
                 test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
                 type: 'asset/resource',
             },
-            // шрифты и SVG
             {
                 test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
                 type: 'asset/inline',
@@ -101,12 +115,8 @@ module.exports = {
             {
                 test: /\.(scss|css)$/i,
                 use: [
-                  // Creates `style` nodes from JS strings
                   "style-loader",
-                  //MiniCssExtractPlugin.loader,
-                  // Translates CSS into CommonJS
                   "css-loader",
-                  // Compiles Sass to CSS
                   "sass-loader",
                     {
                         loader: 'sass-resources-loader',
